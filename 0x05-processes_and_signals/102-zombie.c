d2f188f7099ed60ea99c1758d49ab795939dd47d
smartraysam <raysamtob@gmail.com>
2023-08-01 14:29:04 +0100

    Tast 5 - 102

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int infinite_while(void);
+
+/**
+ * main - Entry point for function to create 5 zombie processes
+ * Return: infinite_while loop
+ */
+int main(void)
+{
+	pid_t PID;
+	unsigned int i;
+
+	while(i < 5)
+	{
+		PID = fork();
+		if (PID == 0)
+			exit(0);
+		else
+			printf("Zombie process created, PID: %d\n", PID);
+		i++;
+	}
+	return (infinite_while());
+}
+
+/**
+ * infinite_while - create infinite sleep loop
+ * Return: 0
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
